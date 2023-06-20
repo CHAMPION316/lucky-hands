a86b094ca15a9ec3a168524f0d6f543c5ae6e79d
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-06-21 00:54:52 +0200

    Create variables for computer and user choices

--- run.py
+++ run.py
@@ -28,3 +28,14 @@ scissors = '''
 ---.__(___)  
 '''  
 
+#user choice
+user_action = input("Enter a choice (rock, paper, scissors): ")
+
+#possible outcomes
+possible_actions = ["rock", "paper", "scissors"]
+
+#computer choice
+computer_action = random.choice(possible_actions)
+
+#print statement for results
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
