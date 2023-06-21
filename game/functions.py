1680cf83ee93e47f9b1b1ca90b82788a524e3dd7
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-06-22 01:40:26 +0200

    create home_screen function and imports

---
+++ game/functions.py
@@ -0,0 +1,14 @@
+""" Imports for game functionality """
+import random
+import operator
+import sys
+
+from .utils import delay_print, clear_terminal
+
+def home_screen():
+    """
+    Home screen options and inputs
+    for opening game
+    """
+    clear_terminal()
+    
