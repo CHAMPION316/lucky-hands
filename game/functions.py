70d6fd3a6b6a01f5bd57010b11b69740afc34eed
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-06-24 20:23:48 +0200

    Create main() function for games logic in functions.py

--- game/functions.py
+++ game/functions.py
@@ -4,7 +4,7 @@ import operator
 import keyboard
 import sys
 
-from .drawings import logo, game_images
+from .drawings import logo, rock, paper, scissors
 from .utils import delay_print, clear_terminal
 
 
@@ -18,17 +18,28 @@ def home_screen():
     print("\n" * 2)
     delay_print("{:^70}".format(input("PRESS ENTER TO PLAY")), 1)
     clear_terminal()
-    game_choices = game_images
-    for choice in game_choices:
-        print(choice)
     
 # (Here I will probably create a def play() section when ready)
+
+def play_game():
+    """
+    Game logic user choice and
+    computer choice
+    """
+    # Game options
+    game_choices = [rock, paper, scissors]
+    # User choice
+    user_choice = int(input("What do you choose? Type 0 for rock, 1 for paper, 2 for scissor. \n => "))
+    print("User Choice: ")
+    print(game_choices[user_choice])
+    
     
 def main():
     """
     The main game loop
     """
     home_screen()
+    play_game()
     
 
 if __name__ == "__main__":
