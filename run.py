001b97585f99686677c8adb9379c1296d2c51682
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-06-24 18:25:07 +0200

    Import game.functions to run.py

--- run.py
+++ run.py
@@ -1,19 +1,3 @@
-import random
-
-
-# #user choice
-# user_action = input("Enter a choice (rock, paper, scissors): ")
-
-# #possible outcomes
-# possible_actions = ["rock", "paper", "scissors"]
-
-# #computer choice
-# computer_action = random.choice(possible_actions)
-
-# #print statement for results
-# print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
-
-
 from game.functions import main
 
 main()
