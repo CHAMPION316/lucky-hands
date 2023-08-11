649946840246c9cc2c5c3e3dec6591136d5583bb
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-08-11 12:28:59 +0200

    Fix 'def update_scores' for score keeping

--- game/functions.py
+++ game/functions.py
@@ -42,7 +42,7 @@ def home_screen():
             print("{:^70}".format("Please Choose option 1, 2 or 3"))
             
 
-#FIX INFINITE LOOP WITH CHOICE 1 - takes you back to home_screen and starts all over.
+# FIX INFINITE LOOP WITH CHOICE 1 - takes you back to home_screen and starts all over.
 # Maybe do best of 5 or something then end game or choose to play again
 def play_game():
     """
@@ -50,35 +50,55 @@ def play_game():
     computer choice
     """
     clear_terminal()
+    
+    # Initialize scores
+    user_score = 0
+    computer_score = 0
+    
     # Game options
     game_choices = [rock, paper, scissors]
     
-    #User Choice logic
-    while True:
-        try:
-            # User Choice
-            user_choice = int(input("Type 0 for rock, 1 for paper, 2 for scissor. \n => "))
+    # Play until someone reaches 5 points
+    while user_score < 5 and computer_score < 5:
+        #User Choice logic
+        while True:
+            try:
+                # User Choice
+                user_choice = int(input("Type 0 for rock, 1 for paper, 2 for scissor. \n => "))
+                
+                if user_choice not in [0, 1, 2]:
+                    print("Invalid integer please choose 0, 1, or 2")
+                else:
+                    break # Break the loop if the input is valid
             
-            if user_choice not in [0, 1, 2]:
-                print("Invalid integer please choose 0, 1, or 2")
-            else:
-                break # Break the loop if the input is valid
+            except ValueError:
+                print("Invalid input. Please choose a number.")
         
-        except ValueError:
-            print("Invalid input. Please choose a number.")
-    
-     # print user choice
-    print("User Choice: ", game_choices[user_choice])
-    
-    
-    # Computer Choice
-    computer_choice = random.randint(0, 2)
-    
-    #print computer choice
-    print("Computer Choice: ", game_choices[computer_choice])
-    
-    # Call the hand_choice_text funtion texts 
-    choice_text = hand_name_text(user_choice, computer_choice)
+        # Computer Choice
+        computer_choice = random.randint(0, 2)
+        
+        # print user choice / computer choice
+        print("User Choice: ", game_choices[user_choice])
+        print("Computer Choice: ", game_choices[computer_choice])
+        
+        # Call the hand_choice_text funtion texts 
+        choice_text = hand_name_text(user_choice, computer_choice)
+        
+        # Update scores based on the outcome
+        user_score, computer_score = update_scores(user_choice, computer_choice, user_score, computer_score)
+        
+        # Display scores
+        print(f"User Score: {user_score}")
+        print(f"Computer Score: {computer_score}")
+        
+    # Determine and print the winner
+    if user_score > computer_score:
+        print("You Win!")
+    elif user_score < computer_score:
+        print("Computer Wins!")
+    else:
+        print("It's a tie!")        
+        
 
     
 def hand_name_text(user_choice, computer_choice):
@@ -112,17 +132,19 @@ def update_scores(user_choice, computer_choice, user_score, computer_score,):
     Update scores based on the
     outcome of the round
     """
-    # Game rules to determine the winner
     if user_choice == computer_choice:
+        # Draw, no score change
         return user_score, computer_score # Draw no score change
     elif (user_choice == 0 and computer_choice == 2) or \
          (user_choice == 1 and computer_choice == 0) or \
          (user_choice == 2 and computer_choice ==1):
-        user_choice +=1
+        # User wins the round, update user's score
+        user_score +=1
     else:
-        computer_choice +=1
+        # Computer wins the round, update computer's score
+        computer_score +=1
         
-    return user_choice, computer_choice
+    return user_score, computer_score
 
    
 # Game rules explained - option 3 in home_screen()
